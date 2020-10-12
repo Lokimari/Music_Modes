f744def9931b45445529e4c6a4fbbbe56f1d382b
fef <nhbb920@gmail.com>
2020-10-11 21:26:51 -0500

    Import data variables from data.py + prettify

--- functions.py
+++ functions.py
@@ -1,55 +1,13 @@
+import data as dt
 import copy
 
-# Data
-musical_alphabet = ["A",  "A#", "B",  "C", "C#", "D",  "D#", "E",  "F", "F#", "G", "G#",
-                    "A",  "A#", "B",  "C", "C#", "D",  "D#", "E",  "F", "F#", "G", "G#"]
-#           indices: 0     1     2     3    4     5     6     7     8    9     10   11
-flats = ["Ab", "Bb", "Cb", "Db", "Eb", "Fb", "Gb"]
-
-W = 2
-H = 1
-
-# Modes and their steps, final step will be shown if scale_length is set to 7 below.
-mode_dictionary = {
-    "Ionian        ": [W, W, H, W, W, W, H],
-    "Dorian        ": [W, H, W, W, W, H, W],
-    "Phrygian      ": [H, W, W, W, H, W, W],
-    "Lydian        ": [W, W, W, H, W, W, H],
-    "Mixolydian    ": [W, W, H, W, W, H, W],
-    "Aeolian       ": [W, H, W, W, H, W, W],
-    "Locrian       ": [H, W, W, H, W, W, W],
-    "Harmonic Major": [W, W, H, W, H, W + H, W],
-    "Harmonic Minor": [W, H, W, W, H, W + H, H],
-    "Melodic Major ": [W, W, H, W, H, W, W],
-    "Melodic Minor ": [W, H, W, W, W, W, H],
-}
-
-mode_list_no_spaces = [
-    "Ionian",
-    "Dorian",
-    "Phrygian",
-    "Lydian",
-    "Mixolydian",
-    "Aeolian",
-    "Locrian",
-    "Harmonic Major",
-    "Harmonic Minor",
-    "Melodic Major",
-    "Melodic Minor"
-]
-
-mode_list = list(mode_dictionary)
-
-chord_types_dictionary = {
-    "Major": [1, 3, 5],
-    "Minor": [1, 2, 5],
-    # "Diminished": [1, 2, 4],
-    # "Augmented": [1, 3, 6],
-    # Add more later, these are enough for testing...
-}
-
-scale_length = 6  # Does not wrap to root note
-
+# Variables
+musical_alphabet = dt.musical_alphabet
+mode_dictionary = dt.mode_dictionary
+scale_length = dt.scale_length
+mode_list = dt.mode_list
+mode_list_no_spaces = dt.mode_list_no_spaces
+chord_types_dictionary = dt.chord_types_dictionary
 
 # Functions
 def number_from_scale_input(scale_input) -> int:
@@ -93,7 +51,7 @@ def print_all_modes_for_each_semitone() -> None:
         for mode in range(len(mode_dictionary)):
             steps = []
 
-            # Creating the scale basesd on mode
+            # Creating the scale based on mode
             for note in range(scale_length):
                 step_value = mode_dictionary[mode_list[mode]][note]
                 root_value = number_from_scale_input(root_scale)
